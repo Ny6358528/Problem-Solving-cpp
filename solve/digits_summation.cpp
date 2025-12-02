64e1854b03e5ecf9158a5283c6628028e3c992cd
nada yasser <ny6358528@gmail.com>
2025-12-02 10:25:46 +0200

    Digits Summation

---
+++ solve/digits_summation.cpp
@@ -0,0 +1,14 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+ 
+int main() {
+   long long a,b;
+   cin>>a>>b;
+   cout<<a%10+b%10;
+   
+    return 0;
+   
+}
