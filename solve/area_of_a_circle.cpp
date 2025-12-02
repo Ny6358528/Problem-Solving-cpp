e60b5aef9689c482230360420d9f87ef657bad84
nada yasser <ny6358528@gmail.com>
2025-12-02 10:20:48 +0200

    Area of a Circle+Difference

---
+++ solve/area_of_a_circle.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include<iomanip>
+using namespace std;
+ 
+ 
+int main() {
+    cout << fixed << setprecision(9);
+ /*
+ Area = π * R2.
+
+Note: consider π = 3.141592653.
+ */
+   double r;
+   cin>>r;
+   double area=3.141592653*r*r;
+   cout<<area;
+ 
+ 
+   
+    return 0;
+   
+}
---
+++ solve/difference.cpp
@@ -0,0 +1,16 @@
+
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+ 
+int main() {
+   // X = (A * B) - (C * D).
+   long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    long long x=(a*b)-(c*d);
+    cout<<"Difference = "<<x;
+    return 0;
+   
+}
