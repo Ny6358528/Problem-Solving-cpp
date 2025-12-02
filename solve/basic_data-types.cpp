1d951a3f4a0fa9542fc28e30ae14fd3e972a3121
nada yasser <ny6358528@gmail.com>
2025-12-02 09:59:06 +0200

    Basic Data Types

---
+++ solve/basic_data-types.cpp
@@ -0,0 +1,21 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+ 
+int main() {
+    //3 12345678912345 a 334.23 14049.30493
+
+   int num;
+   long long long_num;
+    char ch;
+    float f;
+    double d;
+    cin>>num>>long_num>>ch>>f>>d;
+    cout<<num<<endl<<long_num<<endl<<ch<<endl<<f<<endl<<d;
+   
+   
+    return 0;
+   
+}
--- solve/say_hello_with_Cpp.cpp
+++ solve/say_hello_with_Cpp.cpp
@@ -1,5 +1,7 @@
-#include <bits/stdc++.h>
 
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
  
  
