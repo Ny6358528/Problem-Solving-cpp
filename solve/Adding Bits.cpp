32cada68d7e929914ed4b12efe94c24bb9b4d736
nada yasser <ny6358528@gmail.com>
2025-12-07 15:25:55 +0200

    Adding Bits

---
+++ solve/Adding Bits.cpp
@@ -0,0 +1,17 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include<iomanip>
+using namespace std;
+ 
+ 
+int main() {
+   long long a,b;
+    cin>>a>>b;
+
+   unsigned long long res=a^b;
+    cout<<res;
+   
+    return 0;
+   
+}
