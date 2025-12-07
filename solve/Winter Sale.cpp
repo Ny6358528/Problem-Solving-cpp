c36b3e8abaf79a4eb5f31f8e2536fff81b86bf4e
nada yasser <ny6358528@gmail.com>
2025-12-07 13:11:49 +0200

    Winter Sale

---
+++ solve/Winter Sale.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+ 
+ 
+int main() {
+
+long long a,b;
+cin>>a>>b;
+double dis=100-a;
+double dis1=dis/100;
+double res=b/dis1;
+cout<<fixed;
+cout.precision(2);
+cout<<res;
+
+
+   
+    return 0;
+   
+}
