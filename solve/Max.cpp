e40ef9eab739898b5c459eaa344cbf2cd8d8703a
nada yasser <ny6358528@gmail.com>
2025-12-24 12:18:30 +0200

    Max

---
+++ solve/Max.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    long long n;
+    cin >> n;
+
+    long long x, mx=0;
+  
+
+    for(int i = 1; i < n; i++){
+        cin >> x;
+        if(x > mx){
+            mx = x;
+        }
+    }
+
+    cout << mx << endl;
+    return 0;
+}
