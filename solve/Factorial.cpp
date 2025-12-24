6e509df7d4cd7b7f1efc4b6f2e4294643f442c2d
nada yasser <ny6358528@gmail.com>
2025-12-24 12:38:24 +0200

    Factorial

---
+++ solve/Factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while(T--) {
+        long long n;
+        cin >> n;
+
+        long long fact = 1;
+        for(int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        cout << fact << endl;
+    }
+
+    return 0;
+}
