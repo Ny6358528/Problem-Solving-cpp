2a0f8d423f3cc341c9aa22c37983a73b50af01a6
nada yasser <ny6358528@gmail.com>
2025-12-24 12:03:28 +0200

    Fixed Password

---
+++ solve/Fixed Password.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+long long n;
+
+while(true){
+    cin>>n;
+    if(n==1999){
+        cout<<"Correct"<<endl;
+        break;
+    }else{
+        cout<<"Wrong"<<endl;
+       
+    }
+}
+}
+
+
