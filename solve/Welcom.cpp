24cafdd46dce157c3509b10012d2379b32776a1a
nada yasser <ny6358528@gmail.com>
2026-01-15 14:32:25 +0200

    welcome

---
+++ solve/Welcom.cpp
@@ -0,0 +1,19 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+ 
+ 
+int main() {
+string s;
+cin>>s;
+
+  cout<<"Welcome to "<<s;
+
+ 
+   
+    return 0;
+
+   
+}
