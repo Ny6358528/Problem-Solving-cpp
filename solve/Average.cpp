eff99f556cd854e1f417aa8868ed3623a4abf5ed
nada yasser <ny6358528@gmail.com>
2026-01-15 14:48:28 +0200

    Average

---
+++ solve/Average.cpp
@@ -0,0 +1,20 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+ 
+ 
+
+int main() {
+double a,b,c,d,e;
+cin>>a>>b>>c>>d>>e;
+double x=(a+b+c+d+e)/5.0;
+  cout<<x;
+
+ 
+   
+    return 0;
+
+   
+}
