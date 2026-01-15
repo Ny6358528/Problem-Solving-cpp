9737044c877a337668b27982d2780b3815ad0927
nada yasser <ny6358528@gmail.com>
2026-01-15 16:41:26 +0200

    Calculate F

---
+++ solve/Calculate F.cpp
@@ -0,0 +1,20 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+ 
+ 
+
+int main() {
+long long x,y,z,f;
+cin>>x>>y>>z;
+f=(x*y)-(x*z);
+  cout<<f;
+
+ 
+   
+    return 0;
+
+   
+}
