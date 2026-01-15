efb99ce40a9b2ae3b4615868d0ab4eafc43b552a
nada yasser <ny6358528@gmail.com>
2026-01-15 14:37:08 +0200

    Sum

---
+++ solve/Sum.cpp
@@ -0,0 +1,19 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+ 
+ 
+int main() {
+long long a,b;
+cin>>a>>b;
+
+  cout<<a+b;
+
+ 
+   
+    return 0;
+
+   
+}
