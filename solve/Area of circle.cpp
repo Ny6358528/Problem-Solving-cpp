23d1b18af48ad2f0e12b28cc7e82012239ff247a
nada yasser <ny6358528@gmail.com>
2026-01-16 16:16:05 +0200

    Area of circle

---
+++ solve/Area of circle.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+ 
+ 
+
+int main() {
+cout << setprecision(10) << fixed;
+double r,pi,a;
+cin>>r;
+ pi=3.141592653589793;
+a=pi*r*r;
+cout<<a;
+
+ 
+   
+    return 0;
+
+   
+}
