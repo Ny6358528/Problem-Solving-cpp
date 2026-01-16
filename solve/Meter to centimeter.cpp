6d9b0876e99f54ccbdb7382e327aef3021cd20ea
nada yasser <ny6358528@gmail.com>
2026-01-16 16:22:10 +0200

    Meter to centimeter

---
+++ solve/Meter to centimeter.cpp
@@ -0,0 +1,18 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include<iomanip>
+using namespace std;
+ 
+ 
+int main() {
+
+    long long x,a;
+    string s;
+
+    cin>>x>>s;
+    a=x*100;
+    cout<<a<<" cm";
+    return 0;
+   
+}
