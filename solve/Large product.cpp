5226ec99b76c7f2555c0f4114ac0ecb80463af78
nada yasser <ny6358528@gmail.com>
2026-01-16 16:35:57 +0200

    Large product

---
+++ solve/Large product.cpp
@@ -0,0 +1,16 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+ 
+int main() {
+ long long a,b,r;
+ cin>>a>>b;
+ r=a*b;
+ cout<<r;
+
+    
+    return 0;
+   
+}
