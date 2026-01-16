2b127c90af2c01da44a92fe2634ebb5be1628342
nada yasser <ny6358528@gmail.com>
2026-01-16 16:58:17 +0200

    Concatenate string

---
+++ solve/Concatenate string.cpp
@@ -0,0 +1,16 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include<iomanip>
+using namespace std;
+ 
+ 
+int main() {
+  string s1,s2,s;
+  cin>>s1>>s2;
+  s=s1+s2;
+  cout<<s;
+   
+    return 0;
+   
+}
